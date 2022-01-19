98833789e64954cf675a575d2012f632452e5549
filauro1989 <filauro1989@gmail.com>
2022-01-19 11:41:58 +0100

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "Main",
+    data() {
+        return {
+            queryPath: 'https://api.themoviedb.org/3/search/movie?api_key=f134f75db1d5edf07240f2412a109c60',
+            movies: [],
+        }
+    },
+    methods: {
+        getMovies() {
+            axios.get(this.queryPath)
+            .then((result) => {
+                this.movies = 
+            })
+            .catch()
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+
+</style>
